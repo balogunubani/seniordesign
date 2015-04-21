f2f10dcacd9691ddd85607176ab6d1be339f185c
Ubani Balogun <=balogunubani@gmail.com>
2015-04-21 18:55:31 -0400

    Implemented extendActuator, retractActuator, stopActuator; Wrote basic loop to test functions. 'This is the beginning of the end'

--- solartrackersystem/solartrackersystem.ino
+++ solartrackersystem/solartrackersystem.ino
@@ -1,1 +1,61 @@
+/*
+MEM PR6B Senior Design Project: Solar Lighthouse
+Group members: Ubani Balogun, Joseph Musembi, Ankit Patel, Steven Heidengren, Steve Forest
+Arduino based Solar Tracker System Design
+Author: Ubani Balogun
+
+TODO: Describe Program code
+
+*/
+
+// Actuator definitions
+const int AC_1[2] = {2,3};
+
+// Indexes for Actuator wires
+const int RED = 0;
+const int BLACK = 1;
+
+const int DELAY = 2000;
+// setup
+void setup(){
+  // Setup Actuators
+  pinMode(AC_1[RED],OUTPUT);
+  pinMode(AC_1[BLACK],OUTPUT);
+  
+}
+
+
+void loop(){
+  extendActuator(AC_1);
+  delay(DELAY);
+  stopActuator(AC_1);
+  delay(DELAY);
+  retractActuator(AC_1);
+  delay(DELAY);
+  stopActuator(AC_1);
+  delay(DELAY);
+}
+
+
+// Extend the linear actuator
+void extendActuator(const int actuator[]){
+  digitalWrite(actuator[BLACK],LOW);
+  delay(100);
+  digitalWrite(actuator[RED],HIGH);
+}
+
+
+// Retract the linear actuator
+void retractActuator(const int actuator[]){
+  digitalWrite(actuator[RED],LOW);
+  delay(100);
+  digitalWrite(actuator[BLACK],HIGH);
+}
+
+// Stop linear actuator
+void stopActuator(const int actuator[]){
+  digitalWrite(actuator[RED],LOW);
+  digitalWrite(actuator[BLACK],LOW);
+}
+
 
