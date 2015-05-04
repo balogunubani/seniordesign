bf31c018f017964e94f1b743d7354f27f9f93737
Ubani Balogun <=balogunubani@gmail.com>
2015-05-04 19:43:53 -0400

    Code for Reading Light intensity from Sensor 'Its getting really hot in here' - Nelly, Kinda

---
+++ photoresistortest/photoresistortest.ino
@@ -0,0 +1,41 @@
+/*
+MEM Senior Design PR6B
+@author: Ubani Balogun
+Group members: Ubani Balogun, Joseph Musembi, Steven Heidengren, Steven Forrest, Ankit Patel
+
+Test for the photo resistor sensor used for solar tracking
+Reference: http:/playground.arduino.cc/Learning/PhotoResistor
+Circuit:
+----------------------------------------------------
+
+           PhotoR     10K
+ +5    o---/\/\/--.--/\/\/---o GND
+                  |
+ Pin A0 o----------
+
+----------------------------------------------------
+*/
+#include <stdarg.h>
+
+int photoResistor = A0;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+
+void loop(){
+  printf("%l",analogRead(photoResistor));
+  delay(1000);
+}
+
+
+// Helper Functions
+void printf(char *fmt, ... ){
+        char buf[128]; // resulting string limited to 128 chars
+        va_list args;
+        va_start (args, fmt );
+        vsnprintf(buf, 128, fmt, args);
+        va_end (args);
+        Serial.print(buf);
+}
--- solartrackersystem/solartrackersystem.ino
+++ solartrackersystem/solartrackersystem.ino
@@ -8,32 +8,50 @@ TODO: Describe Program code
 
 */
 
-// Actuator definitions
+#include <Servo.h>
+
+
+// Actuator Definitions
 const int AC_1[2] = {2,3};
+
+// Photoresistor definitions
+const int photoR = A0;
+
 
 // Indexes for Actuator wires
 const int RED = 0;
 const int BLACK = 1;
 
+// Common constants
 const int DELAY = 2000;
+const int DELAY_CALIB = 200;
+
+// Servo Definitions
+Servo myservo;
+int servoPos = 0;
+const int servoPin = 9;
+
 // setup
 void setup(){
   // Setup Actuators
   pinMode(AC_1[RED],OUTPUT);
   pinMode(AC_1[BLACK],OUTPUT);
   
+  // calibrateActuator(AC_1);
+ 
+  // Setup Servo
+  myservo.attach(servoPin);
+
+  // Setup Serial communication for test
+  Serial.begin(9600);
 }
 
 
 void loop(){
-  extendActuator(AC_1);
-  delay(DELAY);
-  stopActuator(AC_1);
-  delay(DELAY);
-  retractActuator(AC_1);
-  delay(DELAY);
-  stopActuator(AC_1);
-  delay(DELAY);
+  int sensorReading;
+  sensorReading = readSensor();
+  Serial.print("Position with Max Intensity: "); Serial.println(sensorReading);
+  delay(1000);
 }
 
 
@@ -58,4 +76,32 @@ void stopActuator(const int actuator[]){
   digitalWrite(actuator[BLACK],LOW);
 }
 
+// Determines the distance Actuator moves in a second
+void calibrateActuator(const int actuator[]){
+  retractActuator(actuator);
+  delay(5000);
+  stopActuator(actuator);
+  extendActuator(actuator);
+  delay(DELAY_CALIB);
+  stopActuator(actuator);
+}
 
+// Sweeps the sensor 180 degrees, reading light intensities and returns the angle of the highest intensity
+int readSensor(){
+  int intens = 0;
+  int maxPos = 0;
+  int finalPos = 0;
+  for (servoPos = 0; servoPos < 180; servoPos+= 10){
+    myservo.write(servoPos);
+    delay(500);
+    
+    if (analogRead(photoR) > intens){
+      intens = analogRead(photoR);
+      // Serial.print("Intens:");Serial.println(intens);
+      finalPos = servoPos;
+      // Serial.print("Intens:");Serial.print(intens);Serial.print(" finalPos:");Serial.println(finalPos);
+    }
+  }
+  myservo.write(finalPos);
+  return finalPos;
+}
